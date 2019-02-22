d8c6cae7495cab0a416351c8fa79433c94999a82
Daniel Esteban Nombela <dani@gatunes.com>
2019-02-23 00:37:29 +0100

    Allow robot scraping

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -189,7 +189,7 @@ module.exports = {
       new RobotstxtPlugin({
         policy: [{
           userAgent: '*',
-          disallow: '/',
+          allow: '/',
         }],
       }),
       new webpack.SourceMapDevToolPlugin({
